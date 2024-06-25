9978014b9a787f940d5b1bc4dca3f3c9655242c7
tantsa148 <159923794+tantsa148@users.noreply.github.com>
2024-06-25 04:20:49 +0200

    file

--- src/annotation/Controller.java
+++
@@ -1,9 +0,0 @@
-package mg.ituprom16.annotation;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Controller {
-    
-}
