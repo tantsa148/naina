d22a1105d6fb930804a03023fce5419969a671f6
tsanta148 <tsantarakotoarimanga14@gmail.com>
2024-10-10 20:08:54 +0300

    Sprint7

--- script.bat
+++ script.bat
@@ -1,84 +1,38 @@
-@ECHO OFF
+@echo off
+setlocal
 
-REM Définir les variables (modifier les valeurs entre guillemets)
+set "work=D:\BOSY\L2\S4\Naina\Sprint-S4"
 
-SET TOMCAT_HOME=E:\xampp\tomcat
-SET APP_NAME=Sprint1
-SET APP_DIR=%~dp0
+set "libpath=C:\xampp\tomcat\lib\servlet-api.jar"
 
-SET SRC_MAIN_DIR=%APP_DIR%src\main\java
-SET SRC_TEST_DIR=%APP_DIR%src\test\java
+set "output=%work%\src"
 
-SET LIB_DIR=%APP_DIR%lib
-SET WEB_DIR=%APP_DIR%src\test\webapp
-SET TEMP_DIR=%APP_DIR%temp
-SET WEBXML_FILE=%APP_DIR%src\test\config\web.xml
-SET WAR_FILE=%APP_DIR%\%APP_NAME%.war
+set "temp=%output%\temp"
 
-REM Créer le répertoire d'applicaion dans Tomcat (vérifier si existant)
-IF EXIST "%APP_DIR%\%APP_NAME%" (
-	ECHO Le répertoire d'applicaion existe déjà. On le supprime.
-	RD /S /Q "%APP_DIR%\%APP_NAME%"
-)
-MKDIR "%APP_DIR%\%APP_NAME%"
-MKDIR "%APP_DIR%\%APP_NAME%\WEB-INF"
-MKDIR "%APP_DIR%\%APP_NAME%\WEB-INF\lib"
-MKDIR "%APP_DIR%\%APP_NAME%\WEB-INF\classes"
+copy "%output%\annotations\*.java" "%temp%"
 
-REM Créer le répertoire d'application dans Tomcat (vérifier si existant)
-IF EXIST "%APP_DIR%\temp" (
-	ECHO Le répertoire d'application existe déjà. On le supprime.
-	RD /S /Q "%APP_DIR%\temp"
-)
-MKDIR "%APP_DIR%\temp"
-MKDIR "%APP_DIR%temp\classes"
-MKDIR "%APP_DIR%temp\java_files"
+copy "%output%\frameworks\*.java" "%temp%"
 
-rem Itérer sur chaque fichier .java dans le répertoire source et ses sous-dossiers
-for /R %SRC_MAIN_DIR% %%a in (*.java) do (
-  rem echo 
-  COPY /Y "%%a" "%APP_DIR%temp\java_files"
-)
+copy "%output%\util\*.java" "%temp%"
 
-for /R %SRC_TEST_DIR% %%a in (*.java) do (
-  echo %%a
-  COPY /Y "%%a" "%APP_DIR%temp\java_files"
-)
+copy "%output%\controllers\*.java" "%temp%"
 
-REM Compiler les classes Java
-javac -parameters -cp %LIB_DIR%\* -d %APP_DIR%temp\classes %APP_DIR%temp\java_files\*.java 
-REM javac -parameters -d %APP_DIR%temp\classes %APP_DIR%temp\java_files\*.java
+javac --source 17 --target 17 -cp "%libpath%" -d "%output%" "%output%\temp\*.java"
 
-REM Copier les classes compilés, le répertoire web et le fichier web.xml
+cd /d "%output%\frameworks"
 
-XCOPY /S /E /Y "%APP_DIR%\temp\classes\*.*" "%APP_DIR%\%APP_NAME%\WEB-INF\classes"
-XCOPY /S /E /Y "%WEB_DIR%\*.*" "%APP_DIR%\%APP_NAME%"
-XCOPY /S /E /Y "%LIB_DIR%\*.*" "%APP_DIR%\%APP_NAME%\WEB-INF\lib"
+jar cvf frontcontrol.jar -C "%output%\frameworks" .
 
-COPY /Y "%WEBXML_FILE%" "%APP_DIR%\%APP_NAME%\WEB-INF"
+copy frontcontrol.jar "%work%\jar"
 
-REM Create a .war file from the temporary directory
-jar cvf "%WAR_FILE%" -C "%APP_DIR%\%APP_NAME%" .
+del frontcontrol.jar
 
-REM Copy the .war file to the webapps directory of Tomcat
-COPY /Y "%WAR_FILE%" "%TOMCAT_HOME%\webapps"
-@REM COPY /Y "%WAR_FILE%" "E:\xampp\tomcat\webapps"
+del "%output%\annotations\*.class"
 
-REM Stop and start Tomcat
-REM %TOMCAT_HOME%\bin\shutdown.bat
-REM %TOMCAT_HOME%\bin\startup.bat
-REM NET STOP Tomcat%TOMCAT_HOME%:~-2%
-REM NET START Tomcat%TOMCAT_HOME%:~-2%
+del "%output%\frameworks\*.class"
 
-REM CD E:\xampp\tomcat\bin
-REM shutdown.bat
-REM startup.bat
+del "%output%\util\*.class"
 
-@REM ECHO Deployment of te application "%APP_NAME%" completed successfully!
+del "%output%\controllers\*.class"
 
-RD /S /Q "%WAR_FILE%"
-RD /S /Q "%APP_DIR%\%APP_NAME%"
-RD /S /Q "%APP_DIR%\temp"
-
-
-
+pause
